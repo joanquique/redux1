226986e2ab27c3ce1bb752952083370f007a9768
Joan Alvarez <joanquique@gmail.com>
2024-10-29 13:40:11 -0600

    U/App.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/features/products/productsSlice.test.js
+++ src/features/products/productsSlice.test.js
@@ -11,7 +11,7 @@ const store = configureStore({
     products: productsReducer,
   },
   middleware: getDefaultMiddleware({
-    serializableCheck: false, // Puedes desactivar esto si necesitas evitar errores relacionados
+    serializableCheck: false, 
   }),
 });
 
